0dd890a59b42becbaec65971305548b501161d33
Tom Joseph <tj67542007@gmail.com>
2026-07-07 13:54:16 -0700

    1st iteration of app ui

--- app ui/app.py
+++ app ui/app.py
@@ -1,7 +1,30 @@
 import streamlit as st
 
-st.title("Ai Study Coach")
+class app:
+    st.title("Ai Study Coach")
 
-st.header("paste your notes")
+    st.header("paste your notes")
 
-st.text_area("notes")
+    notes = st.text_area("notes", placeholder = "paste notes here")
+
+    if st.button('Quiz Generate'):
+    #code for quiz generation 
+        
+        # TODO: add code to put questions into quiz array after quiz genraetion code made 
+        quiz = []
+        
+        answers = []
+        for i, question in enumerate(quiz):
+            answer = st.text_area(question, placeholder = "answer here")
+            answers.append(answer)
+
+    if st.button('Grade Quiz'):
+        #code for grading quiz 
+        pass
+
+    if st.button('weak topics'):
+        #code for showing weak topics
+        pass
+
+
+
