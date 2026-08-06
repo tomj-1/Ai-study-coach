9c6306aa6660f28cb332dcbfdb9b2db6978911b7
Tom Joseph <tj67542007@gmail.com>
2026-08-05 19:24:37 -0700

    added code for a genertae quiz on weak topics button

--- app ui/code/AI.py
+++ app ui/code/AI.py
@@ -53,3 +53,26 @@ class AI:
         feedback = json.loads(response.output_text)
 
         return feedback["graded_quiz"]
+
+    def weak_topic_practice(self, notes, topics):
+            self.notes = notes
+            self.topics = topics 
+            client = OpenAI()
+            response = client.responses.create(
+                model="gpt-5.4-mini",
+                input=f""" Return JSON only: {{"quiz":[{{"question":"","answer":"","topic":"","difficulty":"","user_answer":""}}]}}
+                Create exactly 5 questions from the notes.
+                Focus only on these topics: {topics}
+                Topic = the specific concept tested.
+                Reuse identical topic names for the same concept.
+                Set user_answer to "".
+                Notes: {notes}
+                """,
+                text={"format": {"type": "json_object"}},
+            )
+    
+            # converts json file to list
+            data = json.loads(response.output_text)
+    
+            return data["quiz"]
+    
--- app ui/code/App.py
+++ app ui/code/App.py
@@ -26,15 +26,8 @@ class App:
         st.session_state.quiz = []
 
     if st.button("Quiz Generate"):
+        st.session_state.notes = saved_note
         st.session_state.quiz = ai.generate_quiz(saved_note)
-
-    if st.session_state.quiz:
-        for i, word in enumerate(st.session_state.quiz):
-            question = word["question"]
-            answer = st.text_area(
-                question, placeholder="answer here", key=f"answer_{i}"
-            )
-            word["user_answer"] = answer
 
     if st.button("Grade Quiz"):
         gradedQuiz = ai.grade_quiz(st.session_state.quiz)
@@ -42,9 +35,15 @@ class App:
         st.json(gradedQuiz)
         path = "app ui/data/results.csv"
         df = pd.DataFrame(gradedQuiz)
-        df.to_csv(path, mode="a", header=not os.path.exists(path), index=False)
+        # method for header names since when I delete data on the file it won't make a header next try
+        file_needs_header = not os.path.exists(path) or os.path.getsize(path) == 0
+        df.to_csv(path, mode="a", header=file_needs_header, index=False)
 
+    if "show_weak_topics" not in st.session_state:
+        st.session_state.show_weak_topics = False
     if st.button("weak topics"):
+        st.session_state.show_weak_topics = True
+    if st.session_state.show_weak_topics:
         path = "app ui/data/results.csv"
         if not os.path.exists(path):
             # displays as a red message
@@ -58,4 +57,24 @@ class App:
                 topic_averages = results.groupby("topic")["grade"].mean()
                 # uses boolean filtering to keep topics with averages below 70%
                 weak_topics = topic_averages[(topic_averages * 100) < 70].sort_values()
-                st.dataframe(weak_topics)
+                topic_names = weak_topics.index.tolist()
+
+                selected_topics = []
+                for index, topic in enumerate(topic_names):
+                    checked = st.checkbox(topic, key=f"weak_topic{index}")
+
+                    if checked:
+                        selected_topics.append(topic)
+
+                if st.button("pratice weak topics"):
+                    st.session_state.quiz = ai.weak_topic_practice(
+                        st.session_state.notes, selected_topics
+                    )
+
+    if st.session_state.quiz:
+        for i, word in enumerate(st.session_state.quiz):
+            question = word["question"]
+            answer = st.text_area(
+                question, placeholder="answer here", key=f"answer_{i}"
+            )
+            word["user_answer"] = answer
